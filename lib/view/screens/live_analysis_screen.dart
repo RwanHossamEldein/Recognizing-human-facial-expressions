9a2831dcc600bdabc991a6b1a9bd713ae71ca5f1
Rwan Hossam <rwanhossam0@gmail.com>
2026-01-11 15:07:38 +0200

    update background screen

--- lib/view/screens/live_analysis_screen.dart
+++ lib/view/screens/live_analysis_screen.dart
@@ -26,7 +26,7 @@ class LiveAnalysisScreen extends ConsumerWidget {
             child: imageFile != null
                 ? Image.file(imageFile, fit: BoxFit.cover)
                 : Image.network(
-                    "https://lh3.googleusercontent.com/aida-public/AB6AXuCqaUt86lkALAlaKtZD7M8_m5s3Dj3YgkKU9bpYF4trBm5jvHnkEBbbyCBIcibsB6deblwkcm4eVO3GXBMrymDctmelWETrguPW5eG0UPbcDo7Mh1XofoNfJr2Hh1LB8gtZ0A2Q-jWUeg-T0sZyTXl_l49jeOH6QYhzfkQTDuv-L1LPrNgCGNxU1ue_Zoy6poUBfeBzp1vCU3CK204m0yceqNs3ETii1rgX0rxRi6MJx9zdL4Bht1AIIVRXAjkCRBjt1FFgTc-mKe-Y",
+                    "https://www.cio.com/wp-content/uploads/2025/02/3829539-0-75501800-1740132217-shutterstock_2482705481.jpg?quality=50&strip=all",
                     fit: BoxFit.cover,
                   ),
           ),
@@ -46,7 +46,7 @@ class LiveAnalysisScreen extends ConsumerWidget {
                   },
                 ),
                 const Text(
-                  "LIVE ANALYSIS",
+                  "EMOTION ANALYSIS",
                   style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
